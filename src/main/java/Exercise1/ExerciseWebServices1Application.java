dd2dfe4b7abf222b4364a48ab358e13083b3075e
haim nemir <chimnem@gmail.com>
2024-09-02 14:36:37 +0300

    1

---
+++ src/main/java/Exercise1/ExerciseWebServices1Application.java
@@ -0,0 +1,14 @@
+package Exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseWebServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseWebServices1Application.class, args);
+		System.out.println("hi");
+	}
+
+}
